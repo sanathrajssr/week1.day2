2656c659789aec7173570734c1671ac045f9eb35
sanathrajssr <104908944+sanathrajssr@users.noreply.github.com>
2022-05-04 19:14:02 +0530

    Add files via upload

---
+++ MyMobile.java
@@ -0,0 +1,16 @@
+package Assignmentweek1.day2;
+
+public class MyMobile {
+
+	public static void main(String[] args) {
+		
+		Mobile calling=new Mobile();
+		calling.sendMessage();
+		calling.shareDocument();
+		calling.call();
+		
+		
+
+	}
+
+}
